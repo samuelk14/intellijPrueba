4045b999050b3a63c7034bef4dce1d7c11e9ff5e
samuelk14 <smho-rey14@hotmail.com>
2022-08-26 11:54:33 -0500

    iniciando

---
+++ verificar/src/main/java/co/prueba/error/verificar/VerificarApplication.java
@@ -0,0 +1,13 @@
+package co.prueba.error.verificar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerificarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerificarApplication.class, args);
+	}
+
+}
---
+++ verificar/src/test/java/co/prueba/error/verificar/VerificarApplicationTests.java
@@ -0,0 +1,13 @@
+package co.prueba.error.verificar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerificarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
